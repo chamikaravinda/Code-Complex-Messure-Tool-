c503bf1b4db88c3ee5638cf5d62d99f8dc23fffd
Chamika Ravinda <chamikaravinda@gmail.com>
2019-08-18 04:35:55 +0530

    style added

--- code_complexity_frontend/src/components/analyse/analyse-home.component.js
+++ code_complexity_frontend/src/components/analyse/analyse-home.component.js
@@ -1,69 +1,92 @@
 import React, { Component } from "react";
-import {MDBContainer, MDBFileInput,MDBBtn} from "mdbreact";
+import { MDBContainer, MDBFileInput, MDBBtn,MDBRow,MDBCol } from "mdbreact";
 import swal from "sweetalert";
 import axios from "axios";
 import constants from "../../util/constants";
 import FileUpload from "../upload/file-upload.component";
 
 class AnalyseHome extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      fileId: ""
+    };
 
-        this.state = {
-            fileId : '',
-        }
+    this.onClickCtc = this.onClickCtc.bind(this);
+    this.onClickCs = this.onClickCs.bind(this);
+  }
 
-        this.onClickCtc = this.onClickCtc.bind(this);
-        this.onClickCs = this.onClickCs.bind(this);
+  componentDidMount() {
+    if (!localStorage.getItem("status")) {
+      this.props.history.push("/login");
     }
+    this.setState({
+      fileId: this.props.match.params.id
+    });
+  }
 
-    componentDidMount() {
-        if(!localStorage.getItem('status')){
-            this.props.history.push("/login")
-          }
-        this.setState({
-            fileId : this.props.match.params.id
-        })
-    }
+  onClickCtc() {
+    axios
+      .get(constants.url + "/controlStructure/analyse/" + this.state.fileId)
+      .then(res => {
+        swal("No of if statements in the code " + res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    onClickCtc(){
+  onClickCs() {
+    axios
+      .get(constants.url + "/statmentsize/analyse/" + this.state.fileId)
+      .then(res => {
+        swal("Complexity of a program statement due to size " + res.data);
+        console.log("dss" + res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-        axios.get(constants.url + "/controlStructure/analyse/" + this.state.fileId )
-            .then(res=>{
-                swal("No of if statements in the code " + res.data);
-            })
-            .catch(err=>{
-                console.log(err);
-            })
-    }
-
-    onClickCs(){
-
-        axios.get(constants.url + "/statmentsize/analyse/" + this.state.fileId )
-            .then(res=>{
-                swal("Complexity of a program statement due to size " + res.data);
-                console.log("dss" + res.data);
-            })
-            .catch(err=>{
-                console.log(err);
-            })
-    }
-
-    render() {
-        return(
-            <>
-                <MDBContainer className="w-25 mt-5">
-
-                    <MDBBtn onClick={this.onClickCs}>Complexity due to size (Cs)</MDBBtn>
-                    <MDBBtn onClick={this.onClickCtc}> Complexity due to type of control structures (Ctc)</MDBBtn>
-                    <MDBBtn>Complexity due to nesting of control structures (Cnc) </MDBBtn>
-                    <MDBBtn>Complexity due to inheritance (Ci) </MDBBtn>
-                    <MDBBtn>Complexity due to recursion (Cr) </MDBBtn>
-                </MDBContainer>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <MDBContainer className="w-100 mt-5">
+          <MDBRow>
+            <MDBCol sm="4">
+              <MDBBtn onClick={this.onClickCs}>
+                Complexity due to size (Cs)
+              </MDBBtn>
+            </MDBCol>
+            <MDBCol sm="4">
+              <MDBBtn onClick={this.onClickCtc}>
+                {" "}
+                Complexity due to type of control structures (Ctc)
+              </MDBBtn>
+            </MDBCol>
+            <MDBCol sm="4">
+              {" "}
+              <MDBBtn>
+                Complexity due to nesting of control structures (Cnc){" "}
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol sm="4">
+            <MDBBtn>Complexity due to inheritance (Ci) </MDBBtn>
+            </MDBCol>
+            <MDBCol sm="4">
+              <MDBBtn onClick={this.onClickCtc}>
+                {" "}
+                Complexity due to type of control structures (Ctc)
+              </MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </>
+    );
+  }
 }
 
-export default AnalyseHome
+export default AnalyseHome;
