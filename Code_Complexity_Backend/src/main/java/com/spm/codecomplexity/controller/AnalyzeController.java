f3596a96d4c7089380f3a21494e09991c9b7528f
deshan <deshanshehantha@gmail.com>
2019-09-14 20:13:39 +0530

    total calculated

--- Code_Complexity_Backend/src/main/java/com/spm/codecomplexity/controller/AnalyzeController.java
+++ Code_Complexity_Backend/src/main/java/com/spm/codecomplexity/controller/AnalyzeController.java
@@ -3,6 +3,7 @@ package com.spm.codecomplexity.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.spm.codecomplexity.model.Response;
 import com.spm.codecomplexity.services.InheritenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -49,35 +50,58 @@ public class AnalyzeController {
 	@Autowired
 	ComplexityProgramStatmentService complexityProgramStatmentService;
 
+	private int  totalCs = 0;
+	private int  totalCtc = 0;
+	private int  totalCnc = 0;
+	private int  totalCi = 0;
+	private int  totalTW = 0;
+	private int  totalCps = 0;
+	private int  totalCr = 0;
+
 	@GetMapping("/controlStructure/analyse/{id}")
-	public List<SingleLine> analyseFile( @PathVariable String id ) {
-	
+	public Response analyseFile( @PathVariable String id ) {
+		totalCs = 0;
+		totalCtc = 0;
+		totalCnc = 0;
+		totalCi = 0;
+		totalTW = 0;
+		totalCps = 0;
+		totalCr = 0;
+
+		Response response = new Response();
 		List<SingleLine> list = readfile.readFile(id);
-		
 		try {
 			list=statmentSizeService.calculateComplexityDueToStatmentSize(list);
 			list=controlStuctureService.calculateComplexityDueToControlStructures(list);
-			//list=recursiveService.calculateComplexityDueToRecurtion(list);
-			//pass the list to your service here as done in the above line
 			list = inheritenceService.calculateComplexityDueToInheritenceStructures(list);
-
-
-
-		//pass the list to your service here as done in the above line
-			// make your service read the list line by line and caculate the complexity and set the complexity using given set method
-			//use getStament method in SingleLine object to read the line
-			
-
 			list=nestingControlStructureService.calculateComplexityDueNestingOfControlStructures(list);
 			list=totalWightService.calculateTotalWeight(list);
 			list=complexityProgramStatmentService.calculateComplexityOfProgramStatment(list);
 			list=recursiveService.calculateComplexityDueToRecurtion(list);
 		} catch (Exception e) {
-
 			e.printStackTrace();
 		}
-		
-		return list;
+
+		for(int x=0 ; x < list.size() ; x++) {
+			totalCs += list.get(x).getCs();
+			totalCtc += list.get(x).getCtc();
+			totalCnc += list.get(x).getCnc();
+			totalCi += list.get(x).getCi();
+			totalTW += list.get(x).getTw();
+			totalCps += list.get(x).getCps();
+			totalCr += list.get(x).getCr();
+		}
+
+		response.setList(list);
+		response.setTotalCs(totalCs);
+		response.setTotalCtc(totalCtc);
+		response.setTotalCnc(totalCnc);
+		response.setTotalCi(totalCi);
+		response.setTotalTW(totalTW);
+		response.setTotalCps(totalCps);
+		response.setTotalCr(totalCr);
+
+		return response;
 	}
 	
 }
---
+++ Code_Complexity_Backend/src/main/java/com/spm/codecomplexity/model/Response.java
@@ -0,0 +1,85 @@
+package com.spm.codecomplexity.model;
+
+import java.util.List;
+
+public class Response {
+
+    private List<SingleLine> list;
+    private int  totalCs;
+    private int  totalCtc;
+    private int  totalCnc;
+    private int  totalCi;
+    private int  totalTW;
+    private int  totalCps;
+    private int  totalCr;
+
+    public List<SingleLine> getList() {
+        return list;
+    }
+
+    public void setList(List<SingleLine> list) {
+        this.list = list;
+    }
+
+    public int getTotalCs() {
+        return totalCs;
+    }
+
+    public void setTotalCs(int totalCs) {
+        this.totalCs = totalCs;
+    }
+
+    public int getTotalCtc() {
+        return totalCtc;
+    }
+
+    public void setTotalCtc(int totalCtc) {
+        this.totalCtc = totalCtc;
+    }
+
+    public int getTotalCnc() {
+        return totalCnc;
+    }
+
+    public void setTotalCnc(int totalCnc) {
+        this.totalCnc = totalCnc;
+    }
+
+    public int getTotalCi() {
+        return totalCi;
+    }
+
+    public void setTotalCi(int totalCi) {
+        this.totalCi = totalCi;
+    }
+
+    public int getTotalTW() {
+        return totalTW;
+    }
+
+    public void setTotalTW(int totalTW) {
+        this.totalTW = totalTW;
+    }
+
+    public int getTotalCps() {
+        return totalCps;
+    }
+
+    public void setTotalCps(int totalCps) {
+        this.totalCps = totalCps;
+    }
+
+    public int getTotalCr() {
+        return totalCr;
+    }
+
+    public void setTotalCr(int totalCr) {
+        this.totalCr = totalCr;
+    }
+
+
+
+
+
+
+}
